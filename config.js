640e16310734c27058b9adbf54ba10c7d2d793bf
donodu <donovan.reseaux@gmail.com>
2026-08-07 03:11:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
   Ne mets jamais ici : service_role, secret key, Client Secret Discord ou token de bot.
 */
 window.SP_CONFIG = {
-  supabaseUrl: "https://takthdfraipiztzoyxds.supabase.co",
+  supabaseUrl: "https://zbmshbfzjprmnvlrfxuh.supabase.co",
   supabaseAnonKey: "sb_publishable_LM0Rqjj9WeANvJxYzZnPcQ_WuFex_Yw",
   siteName: "Silver Phoenix Command Center"
 };
